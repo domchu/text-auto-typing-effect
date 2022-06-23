c737c0bab8a12c89ffd57a547979af5965f47379
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-06-24 00:39:16 +0100

    added the more texts

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const typed = new Typed(".auto-type", {
   strings: [
-    "Ochu, Dominic Onah",
-    "a Front-End Developer",
-    "Coding for a living ",
+    "Ochu, Dominic Onah.",
+    "a Front-End Engineer[Developer].",
+    "a Creative and immovable Front-End Developer with over 2 years Experience building websites and Apps in fast-paced. ",
   ],
   typeSpeed: 150,
   backSpeed: 150,
