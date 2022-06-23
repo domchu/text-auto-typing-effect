bc24f6916815d72db6bc65390bf85f01dc877233
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-06-24 00:08:46 +0100

    added the scripting text

---
+++ app.js
@@ -0,0 +1,10 @@
+const typed = new Typed(".auto-type", {
+  strings: [
+    "Ochu, Dominic Onah",
+    "a Front-End Developer",
+    "Coding for a living ",
+  ],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
