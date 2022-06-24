070bb110e1db9438f520f3b1f02de914cc90dbcc
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-06-24 18:19:08 +0100

    added the media query

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const typed = new Typed(".auto-type", {
     "a Front-End Engineer[Developer].",
     "a creative and immovable front-End developer with over 2 years experience in building websites and apps in fast-paced. ",
   ],
-  typeSpeed: 150,
+  typeSpeed: 300,
   backSpeed: 150,
   loop: true,
 });
